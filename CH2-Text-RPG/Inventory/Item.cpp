a12ad054e96eef7befb67abf3cb4c8f6e13586eb
CherryCereal <a2002seo@gmail.com>
2026-07-29 15:10:29 +0900

    Update Item.cpp

--- CH2-Text-RPG/Inventory/Item.cpp
+++ CH2-Text-RPG/Inventory/Item.cpp
@@ -1,6 +1,6 @@
 #include "Item.h"
 
-Item::Item()
+Item::Item(int id)
 {
 }
 
@@ -9,24 +9,14 @@ Item::~Item()
     // TODO: Implement item destruction.
 }
 
+const int& Item::GetId() const
+{
+
+}
+
 const std::string& Item::GetName() const
 {
     // TODO: Return the item name.
-    return Name;
+    return;
 }
 
-void Item::SetName(const std::string& name)
-{
-    // TODO: Set the item name.
-}
-
-int Item::GetPrice() const
-{
-    // TODO: Return the item price.
-    return {};
-}
-
-void Item::SetPrice(int price)
-{
-    // TODO: Set the item price.
-}
