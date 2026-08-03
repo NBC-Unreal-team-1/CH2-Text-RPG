db4721d4d6f5c7b7eef8869b4fc4a65965bc4280
한성준 <gggf0817@naver.com>
2026-08-03 11:38:39 +0900

    ✨ feat: add ASCII art for each monster

--- CH2-Text-RPG/Character/Monster.cpp
+++ CH2-Text-RPG/Character/Monster.cpp
@@ -172,6 +172,142 @@ int Monster::GetId() const
     return FirstMonsterId + static_cast<int>(MonsterId);
 }
 
+const std::string& Monster::GetAsciiArtById(int MonsterId)
+{
+    static const std::string LettuceArt = R"ART(
+       .-~~~~-.
+    .-'  .--.  '-.
+   /  .-( () )-.  \
+  (  (   '--'   )  )
+   '._'-.____.-'_.'
+      '--------')ART";
+
+    static const std::string OnionArt = R"ART(
+         /\
+        /  \
+      .'    '.
+     /  o  o  \
+    |    --    |
+     \  ____  /
+      '------')ART";
+
+    static const std::string PickleArt = R"ART(
+       __________
+     /  o  o  o  \
+    / o  .----. o \
+   |  o / ^  ^ \  |
+   | o  \  --  / o|
+    \__o_'----'_o_/)ART";
+
+    static const std::string TomatoArt = R"ART(
+        \  |  /
+      ---\ | /---
+         \|/
+      .-~~~~~-.
+     /  o   o  \
+    |     ^     |
+     \  '---'  /
+      '-------')ART";
+
+    static const std::string KetchupArt = R"ART(
+        _______
+       /_______\
+       |  RED  |
+      /|       |\
+     / |  o o  | \
+    |  |   >   |  |
+     \_|__===__|_/)ART";
+
+    static const std::string MayonnaiseArt = R"ART(
+        _______
+       /  MAYO \
+      /_________\
+      |  .---.  |
+      | ( o o ) |
+      |  \_-_/  |
+      '---------')ART";
+
+    static const std::string BaconArt = R"ART(
+      __/\/\/\/\__
+     /  /\/\/\/\  \
+    |  |  o  o  |  |
+    |  |   __   |  |
+     \  \/\/\/\/  /
+      '--/\/\/\--')ART";
+
+    static const std::string CheeseArt = R"ART(
+          /|
+         / |
+        / o|
+       /___|
+      | o  |
+      |  o |
+      |____|)ART";
+
+    static const std::string BreadArt = R"ART(
+       .----------.
+      /  .------.  \
+     /  / o  o  \  \
+    |  |    ^    |  |
+    |  |  '---'  |  |
+     \  '------'  /
+      '----------')ART";
+
+    static const std::string PattyArt = R"ART(
+       .----------.
+      /~~~~~~~~~~~~\
+     |  =========  |
+     |   o      o   |
+     |      __      |
+      \============/
+       '----------')ART";
+
+    static const std::string PotatoBossArt = R"ART(
+          _/\_
+       .-'____'-.
+      /  / /\ \  \
+     |  |  ><  |  |
+     |  | \__/ |  |
+      \  \____/  /
+       '._||||_.'
+        /_||||_\)ART";
+
+    static const std::string UnknownArt = R"ART(
+       .-------.
+      /  ?   ?  \
+     |     ?     |
+      \  _____  /
+       '-------')ART";
+
+    switch (MonsterId)
+    {
+    case 501:
+        return LettuceArt;
+    case 502:
+        return OnionArt;
+    case 503:
+        return PickleArt;
+    case 504:
+        return TomatoArt;
+    case 505:
+        return KetchupArt;
+    case 506:
+        return MayonnaiseArt;
+    case 507:
+        return BaconArt;
+    case 508:
+        return CheeseArt;
+    case 509:
+        return BreadArt;
+    case 510:
+        return PattyArt;
+    case 511:
+        return PotatoBossArt;
+    default:
+        return UnknownArt;
+    }
+}
+
 MonsterType Monster::GetMonsterId() const
 {
     return MonsterId;
